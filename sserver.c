6c7b3fda3ad36f19ce13776ceabbceb23174009c
Aaron <aaronworsnop@gmail.com>
2024-03-16 19:39:54 +0900

    feat: Create TCP socket on sserver

--- sserver.c
+++ sserver.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <errno.h>
-#include <sys/types.h>          /* See NOTES */
+#include <sys/types.h> /* See NOTES */
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,23 +14,31 @@
 
 #include "macro.h"
 /*--------------------------------------------------------------------------------*/
-int 
-main(const int argc, const char** argv)
+int main(const int argc, const char **argv)
 {
     int i;
     int port = -1;
 
     /* argument parsing */
-    for (i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-p") == 0 && (i+1) < argc) {
-            port = atoi(argv[i+1]);
+    for (i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-p") == 0 && (i + 1) < argc)
+        {
+            port = atoi(argv[i + 1]);
             i++;
         }
     }
-    if (port <= 0 || port > 65535) {
+    if (port <= 0 || port > 65535)
+    {
         printf("usage: %s -p port\n", argv[0]);
         exit(-1);
     }
 
-    // implement your own code
+    // Create a TCP socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockfd < 0)
+    {
+        TRACE("Socket creation failed: %s\n", strerror(errno));
+        exit(-1);
+    }
 }
